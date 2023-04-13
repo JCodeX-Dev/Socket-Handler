ab7f4b55134bb55f69181007a8cdb1b2dae9dee2
Jeremy Sam Joseph <79001730+JCodeX-Dev@users.noreply.github.com>
2023-04-14 00:18:50 +0530

    Update socket object read logic

--- src/main/java/dev/jcodex/sockethandler/service/SocketWriterService.java
+++ src/main/java/dev/jcodex/sockethandler/service/SocketWriterService.java
@@ -4,17 +4,24 @@ import dev.jcodex.sockethandler.model.Packet;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 
 @Data
 @AllArgsConstructor
 final class SocketWriterService {
 
-    private OutputStream stream;
+    private ObjectOutputStream stream;
     private Connection connection;
 
-    void write(Packet packet) {
+    public SocketWriterService(OutputStream stream, Connection connection) throws IOException {
+        this.stream = new ObjectOutputStream(stream);
+        this.connection = connection;
+    }
 
+    void write(Packet packet) throws IOException {
+        stream.writeObject(packet);
     }
 
 }
