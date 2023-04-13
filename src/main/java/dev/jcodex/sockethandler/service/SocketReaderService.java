cf1d351d6b563005b2c920b72f40bc498f373407
Jeremy Sam Joseph <79001730+JCodeX-Dev@users.noreply.github.com>
2023-04-14 00:18:35 +0530

    Update socket object read logic

--- src/main/java/dev/jcodex/sockethandler/service/SocketReaderService.java
+++ src/main/java/dev/jcodex/sockethandler/service/SocketReaderService.java
@@ -3,34 +3,42 @@ package dev.jcodex.sockethandler.service;
 import dev.jcodex.sockethandler.model.Packet;
 import lombok.AllArgsConstructor;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 @AllArgsConstructor
 final class SocketReaderService implements Runnable {
 
-    private InputStream stream;
+    private ObjectInputStream stream;
     private Connection connection;
+
+    public SocketReaderService(InputStream stream, Connection connection) throws IOException {
+        this.stream = new ObjectInputStream(stream);
+        this.connection = connection;
+    }
 
     @Override
     public void run() {
 
         while (true) {
             //read data logic
-            String data = "";
+            Packet packet;
+            try {
+                packet = (Packet) stream.readObject();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
 
 
             //if data is read
             //pass it to Connection service
-
-            Packet packet = convertStringToPacket(data);
             connection.readData(packet);
         }
 
-    }
-
-    private Packet convertStringToPacket(String data) {
-
-        return null;
     }
 
 
